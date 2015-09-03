a07db2d6eb6a13e0bd0840a0f7b2a7cd4100c441
shahidash <shahidashraff@gmail.com>
2015-09-03 17:44:07 +0530

    for new version name

--- install_cassandra_cluster.py
+++ install_cassandra_cluster.py
@@ -3,7 +3,7 @@ from fabric.contrib.files import exists
 from fabric.api import env
 import sys
 from StringIO import StringIO
-__author__ = "shahid@trialx.com"
+__author__ = "   shahid@trialx.com"
 
 config_file = """
 
